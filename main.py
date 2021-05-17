d201739f32393416da50fd02cc63ae428a1530d9
Алексиев <kadoll555@gmail.com>
2021-05-17 21:55:28 +0300

    pushing pixels

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 
 TOKEN = os.getenv("TOKEN")
 USERNAME = os.getenv("USERNAME")
+pixel_table_header = {
+    "X-USER-TOKEN": TOKEN,
+}
 
 pixela_endpoint = "https://pixe.la/v1/users"
 acc_registration_args = {
@@ -11,7 +14,6 @@ acc_registration_args = {
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
-
 pixela_graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 pixel_table_args = {
     "id": "running",
@@ -20,9 +22,11 @@ pixel_table_args = {
     "type": "float",
     "color": "kuro",
 }
-pixel_table_header = {
-    "X-USER-TOKEN": TOKEN,
+pixela_push_pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{pixel_table_args['id']}"
+pixela_push_pixel_args = {
+    "date": "20210411",
+    "quantity": "12",
 }
 
-response = requests.post(url=pixela_graph_endpoint, json=pixel_table_args, headers=pixel_table_header)
+response = requests.post(url=pixela_push_pixel_endpoint, json=pixela_push_pixel_args, headers=pixel_table_header)
 print(response.text)
